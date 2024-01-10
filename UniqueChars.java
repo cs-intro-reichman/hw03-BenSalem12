8693833045ee6e6f6542cc31324b34eebef819a0
BenSalem12 <154272684+BenSalem12@users.noreply.github.com>
2024-01-11 00:05:38 +0200

    Add files via upload

--- UniqueChars.java
+++ UniqueChars.java
@@ -11,7 +11,12 @@ public class UniqueChars {
      * unless they are space characters.
      */
     public static String uniqueChars(String s) {
-        // Replace the following statement with your code
-        return null;
+		String newStr = "";
+        for (int i = 0; i < s.length(); i++){
+			if ((s.charAt(i) == ' ') || (s.indexOf(s.charAt(i)) == i)){
+				newStr += s.charAt(i);
+			}
+		}		
+        return newStr;
     }
 }
