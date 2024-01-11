3cd13047f97ac954f85a9d72aa591679ef136573
BenSalem12 <154272684+BenSalem12@users.noreply.github.com>
2024-01-11 17:21:06 +0200

    Add files via upload

--- Calendar.java
+++ Calendar.java
@@ -1,7 +1,7 @@
 /** 
  * Prints the calendars of all the years in the 20th century.
  */
-public class Calendar1 {	
+public class Calendar {	
     // Starting the calendar on 1/1/1900
 	static int dayOfMonth = 1;   
 	static int month = 1;
@@ -18,27 +18,33 @@ public class Calendar1 {
 	    // Prints each date dd/mm/yyyy in a separate line. If the day is a Sunday, prints "Sunday".
 	    // The following variable, used for debugging purposes, counts how many days were advanced so far.
 	    int debugDaysCounter = 0, numSundays = 0;
+		int theYear = Integer.parseInt(args[0]);
 	    //// Write the necessary initialization code, and replace the condition
 	    //// of the while loop with the necessary condition 
-	 	while (year <= 1999) {
-	 		//// Write the body of the while
+	 	while (year < theYear) {
+			if (dayOfWeek == 1) {
+               if (dayOfMonth == 1) {
+                   numSundays = numSundays + 1;
+               }
+            }			
+	 		advance();
+	 		//// If you want to stop the loop after n days, replace the condition of the
+	 		//// if statement with the condition (debugDaysCounter == n)
+        }
+		
+		while (year == theYear) {
 			if (dayOfWeek == 1) {
                if (dayOfMonth == 1) {
                    numSundays = numSundays + 1;
                }
                System.out.println(dayOfMonth+"/"+month+"/"+year+" Sunday");
-            }else{
+            }else {
                System.out.println(dayOfMonth+"/"+month+"/"+year);
             }			
 	 		advance();
-	 		debugDaysCounter++;
 	 		//// If you want to stop the loop after n days, replace the condition of the
 	 		//// if statement with the condition (debugDaysCounter == n)
-	 		if (debugDaysCounter == 10) { 
-	 			break;
-	 		}
         }
-	 	System.out.println("During the 20th century, "+numSundays+" Sundays fell on the first day of the month");
 	 }
 	
 	 // Advances the date (day, month, year) and the day-of-the-week.
